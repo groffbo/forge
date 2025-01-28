a5c5f01fd7b92252f73ddc1f3d1de1b8839f7d3d
anna-sage-local <anna.macinnis109@gmail.com>
2025-01-28 10:18:18 -0500

    empty member data component

---
+++ apps/blade/src/app/admin/data/_components/MemberData.tsx
@@ -0,0 +1,9 @@
+
+
+export default function MemberData() {
+    return (
+        <div>
+            member data here
+        </div>
+    );
+}
--- apps/blade/src/app/admin/data/page.tsx
+++ apps/blade/src/app/admin/data/page.tsx
@@ -1,4 +1,5 @@
 import { api, HydrateClient } from "~/trpc/server";
+import MemberData from "./_components/MemberData";
 
 export default function Data() {
     return (
@@ -9,6 +10,7 @@ export default function Data() {
                         <h1 className="text-3xl font-extrabold tracking-tight">
                             Member Demographics
                         </h1>
+                        <MemberData />
                     </div>
                     <div className="flex flex-col">
                         <h1 className="text-3xl font-extrabold tracking-tight">
