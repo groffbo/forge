2a9067dcb5bc43c64c156e2a40e36c6f4535cda1
anna-sage-local <anna.macinnis109@gmail.com>
2025-01-29 20:17:31 -0500

    dues vs no dues dummy data visualization

--- apps/blade/src/app/admin/data/page.tsx
+++ apps/blade/src/app/admin/data/page.tsx
@@ -1,16 +1,16 @@
 import { api, HydrateClient } from "~/trpc/server";
-import MemberData from "./_components/MemberData";
+import MembersDuesRadialChart from "./_components/MembersDuesRadialChart";
 
 export default function Data() {
     return (
         <HydrateClient>
             <main className="container">
                 <div className="grid grid-cols-2 mt-10">
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-8">
                         <h1 className="text-3xl font-extrabold tracking-tight">
                             Member Demographics
                         </h1>
-                        <MemberData />
+                        <MembersDuesRadialChart />
                     </div>
                     <div className="flex flex-col">
                         <h1 className="text-3xl font-extrabold tracking-tight">
