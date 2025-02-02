5b401d2e5502f830ff26c73b3e909d28d9f550ff
anna-sage-local <anna.macinnis109@gmail.com>
2025-02-02 15:13:54 -0500

    gender distribution pie chart

--- apps/blade/src/app/admin/data/_components/MemberDemographics.tsx
+++ apps/blade/src/app/admin/data/_components/MemberDemographics.tsx
@@ -2,16 +2,22 @@
 
 import { api } from "~/trpc/react";
 import DuesPercentCard from "./DuesPercentCard";
+import GenderPieChartCard from "./GenderPieChartCard";
 
 export default function MemberDemographics() {
     const { data: members } = api.member.getMembers.useQuery();
     const { data: duesPayingStatus } = api.member.getDuesPayingMembers.useQuery();
 
     return (
-        <div>
+        <div className="my-8">
             {members && duesPayingStatus ? 
-            <DuesPercentCard numMembers={members.length} numDuesPaying={duesPayingStatus.length} /> : 
-            <p>error</p>}
+            <div className="flex flex-col gap-4">
+                <DuesPercentCard numMembers={members.length} numDuesPaying={duesPayingStatus.length} /> 
+                <div className="flex flex-row sm:flex-col">
+                    <GenderPieChartCard members={members} />
+                </div>
+            </div>
+            : <p>error</p>}
         </div>
     );
 }
