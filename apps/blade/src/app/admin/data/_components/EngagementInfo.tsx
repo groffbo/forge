07fd8edc8b71f45ea5bafa63f82e8bd19e95bdc1
anna-sage-local <anna.macinnis109@gmail.com>
2025-02-10 20:56:20 -0500

    average events attended to member demographics

--- apps/blade/src/app/admin/data/_components/EngagementInfo.tsx
+++ apps/blade/src/app/admin/data/_components/EngagementInfo.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import type { InsertMember, ReturnEvent } from "@forge/db/schemas/knight-hacks";
 import { Card, CardContent, CardHeader, CardTitle } from "@forge/ui/card";
 
-export default function DuesPercent({numMembers, numDuesPaying} : {numMembers: number, numDuesPaying: number}) {
-    const percentDuesPaying = numDuesPaying / numMembers * 100;
+export default function EngagementInfo({members, events, numDuesPaying} : {members: InsertMember[], events: ReturnEvent[], numDuesPaying: number}) {
+    const percentDuesPaying = numDuesPaying / members.length * 100;
+    const attendances = events.reduce((sum, event) => sum + event.numAttended, 0);
 
     return (
         <Card className="col-span-2">
             <CardHeader>
-                <CardTitle className="text-xl">Dues</CardTitle>
+                <CardTitle className="text-xl">Club Engagement</CardTitle>
             </CardHeader>
             <CardContent>
                 <div className="flex flex-row gap-4">
@@ -20,6 +22,10 @@ export default function DuesPercent({numMembers, numDuesPaying} : {numMembers: n
                         <span className="text-xl text-red-600 font-bold">{100-percentDuesPaying}% </span>
                         <span className="text-muted-foreground">haven't paid dues.</span>
                     </p>
+                </div>
+                <div className="flex flex-row gap-4">
+                    <p>Average Events Attended: {events.length > 0 ? attendances / events.length : 0}</p>
+                    <p>Average Points: 3</p>
                 </div>
             </CardContent>
         </Card>
--- apps/blade/src/app/admin/data/_components/MemberDemographics.tsx
+++ apps/blade/src/app/admin/data/_components/MemberDemographics.tsx
@@ -2,7 +2,7 @@
 
 import { api } from "~/trpc/react";
 import RaceOrEthnicityPie from "./RaceOrEthnicityPie";
-import DuesPercent from "./DuesPercent";
+import EngagementInfo from "./EngagementInfo";
 import GenderPie from "./GenderPie";
 import AgeBarChart from "./AgeBarChart";
 import SchoolYearPie from "./SchoolYearPie";
@@ -10,12 +10,13 @@ import SchoolYearPie from "./SchoolYearPie";
 export default function MemberDemographics() {
     const { data: members } = api.member.getMembers.useQuery();
     const { data: duesPayingStatus } = api.member.getDuesPayingMembers.useQuery();
+    const { data: events } = api.member.getEvents.useQuery();
 
     return (
         <div className="my-8">
             {members && duesPayingStatus && 
             <div className="grid grid-cols-2 gap-4">
-                <DuesPercent numMembers={members.length} numDuesPaying={duesPayingStatus.length} /> 
+                <EngagementInfo members={members} events={events ?? []} numDuesPaying={duesPayingStatus.length} /> 
                 <GenderPie members={members} />
                 <RaceOrEthnicityPie members={members} />
                 <AgeBarChart members={members} />
