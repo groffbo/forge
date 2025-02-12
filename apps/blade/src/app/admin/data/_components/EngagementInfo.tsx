4a33f944424f136bdb18b0a3ef51923085e13388
anna-sage-local <anna.macinnis109@gmail.com>
2025-02-12 09:48:46 -0500

    add average points calculation

--- apps/blade/src/app/admin/data/_components/EngagementInfo.tsx
+++ apps/blade/src/app/admin/data/_components/EngagementInfo.tsx
@@ -6,6 +6,9 @@ import { Card, CardContent, CardHeader, CardTitle } from "@forge/ui/card";
 export default function EngagementInfo({members, events, numDuesPaying} : {members: InsertMember[], events: ReturnEvent[], numDuesPaying: number}) {
     const percentDuesPaying = numDuesPaying / members.length * 100;
     const attendances = events.reduce((sum, event) => sum + event.numAttended, 0);
+    const avgPoints = members.length > 0 ? 
+        members.reduce((sum, member) => sum + (member.points ? member.points : 0), 0) / members.length
+        : 0;
 
     return (
         <Card className="col-span-2">
@@ -25,7 +28,7 @@ export default function EngagementInfo({members, events, numDuesPaying} : {membe
                 </div>
                 <div className="flex flex-row gap-4">
                     <p>Average Events Attended: {events.length > 0 ? attendances / events.length : 0}</p>
-                    <p>Average Points: 3</p>
+                    <p>Average Points: {avgPoints}</p>
                 </div>
             </CardContent>
         </Card>
