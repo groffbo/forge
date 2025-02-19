315204d1e5bfa1bebf1df0146067c4b1b5a701e1
anna-sage-local <anna.macinnis109@gmail.com>
2025-02-19 12:07:47 -0500

    pie chart of events by tag/type

--- apps/blade/src/app/admin/data/_components/EventDemographics.tsx
+++ apps/blade/src/app/admin/data/_components/EventDemographics.tsx
@@ -2,6 +2,7 @@
 
 import { api } from "~/trpc/react";
 import PopularityRanking from "./event-data/PopularityRanking"
+import TypePieChart from "./event-data/TypePieChart";
 
 export default function EventDemographics() {
     const { data: events } = api.event.getEvents.useQuery();
@@ -11,7 +12,7 @@ export default function EventDemographics() {
             {events && 
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
                 <PopularityRanking events={events} />
-                <p>% gbm and % hello world</p>
+                <TypePieChart events={events} />
                 <p>avg num attendees based on type</p>
                 <p>weekday popularity ranking</p>
                 <p>time popularity ranking</p>
---
+++ apps/blade/src/app/admin/data/_components/event-data/TypePieChart.tsx
@@ -0,0 +1,196 @@
+"use client";
+
+import type { PieSectorDataItem } from "recharts/types/polar/Pie";
+import { useMemo, useState } from "react";
+import { Cell, Label, Pie, PieChart, Sector } from "recharts";
+
+import type { ReturnEvent } from "@forge/db/schemas/knight-hacks";
+import type { ChartConfig } from "@forge/ui/chart";
+import { ADMIN_PIE_CHART_COLORS } from "@forge/consts/knight-hacks";
+import { Card, CardContent, CardHeader, CardTitle } from "@forge/ui/card";
+import {
+  ChartContainer,
+  ChartStyle,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@forge/ui/chart";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@forge/ui/select";
+
+export default function TypePieChart({
+  events,
+}: {
+  events: ReturnEvent[];
+}) {
+  const id = "pie-interactive";
+
+  // get amount of each tag
+  const tagCounts: Record<string, number> = {};
+  events.forEach(({ tag }) => {
+    tagCounts[tag] = (tagCounts[tag] ?? 0) + 1;
+  });
+
+  const totalEvents = Object.values(tagCounts).reduce((sum, count) => sum + count, 0);
+  const tagData = Object.entries(tagCounts).map(([tag, count]) => ({
+    name: tag,
+    amount: count,
+    percentage: (totalEvents > 0 ? (count / totalEvents) * 100 : 0).toFixed(2),
+  }));
+
+  const [activeLevel, setActiveLevel] = useState(
+    tagData[0] ? tagData[0].name : null,
+  );
+
+  const activeIndex = useMemo(
+    () => tagData.findIndex((item) => item.name === activeLevel),
+    [activeLevel, tagData],
+  );
+  const tags = useMemo(
+    () => tagData.map((item) => item.name),
+    [tagData],
+  );
+
+  // set up chart config
+  const baseConfig: ChartConfig = {
+    events: { label: "events" },
+  };
+  let colorIdx = 0;
+  events.forEach(({ tag }) => {
+    if (!baseConfig[tag]) {
+      baseConfig[tag] = {
+        label: tag,
+        color: ADMIN_PIE_CHART_COLORS[colorIdx % ADMIN_PIE_CHART_COLORS.length],
+      };
+      colorIdx++;
+    }
+  });
+
+  return (
+    <Card data-chart={id} className="flex flex-col">
+      <ChartStyle id={id} config={baseConfig} />
+      <CardHeader className="flex-col items-start gap-4 space-y-0 pb-0">
+        <div className="grid gap-1">
+          <CardTitle className="text-xl">Event Types</CardTitle>
+        </div>
+        <Select
+          value={activeLevel ? activeLevel : undefined}
+          onValueChange={setActiveLevel}
+        >
+          <SelectTrigger
+            className="ml-auto h-7 rounded-lg pl-2.5"
+            aria-label="Select a value"
+          >
+            <SelectValue placeholder="Select month" />
+          </SelectTrigger>
+          <SelectContent align="end" className="rounded-xl">
+            {tags.map((key) => {
+              const config = baseConfig[key];
+
+              if (!config) {
+                return null;
+              }
+
+              return (
+                <SelectItem
+                  key={key}
+                  value={key}
+                  className="rounded-lg [&_span]:flex"
+                >
+                  <div className="flex items-center gap-2 text-xs">
+                    <span
+                      className="flex h-3 w-3 shrink-0 rounded-sm"
+                      style={{
+                        backgroundColor: config.color,
+                      }}
+                    />
+                    {config.label}
+                  </div>
+                </SelectItem>
+              );
+            })}
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent className="mt-4 flex flex-1 justify-center pb-0">
+        <ChartContainer
+          id={id}
+          config={baseConfig}
+          className="mx-auto aspect-square w-full max-w-[300px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={tagData}
+              dataKey="amount"
+              nameKey="name"
+              innerRadius={60}
+              strokeWidth={5}
+              activeIndex={activeIndex}
+              activeShape={({
+                outerRadius = 0,
+                ...props
+              }: PieSectorDataItem) => (
+                <g>
+                  <Sector {...props} outerRadius={outerRadius + 10} />
+                  <Sector
+                    {...props}
+                    outerRadius={outerRadius + 25}
+                    innerRadius={outerRadius + 12}
+                  />
+                </g>
+              )}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-2xl font-bold"
+                        >
+                          {tagData[activeIndex]?.percentage.toLocaleString()}%
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy ?? 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Events
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+              {tagData.map((_, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={
+                    ADMIN_PIE_CHART_COLORS[
+                      index % ADMIN_PIE_CHART_COLORS.length
+                    ]
+                  }
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
