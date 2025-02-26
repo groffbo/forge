3d0da66d037540f61375e9b5d189f7d8e58dd875
Arthur <arthurteng0819@gmail.com>
2025-02-25 20:48:27 -0500

    commented out jakingit audio until I need it and finished basic navbar design, did not make it link to anything or do anything yet

--- apps/blade/src/app/arthurteng/page.tsx
+++ apps/blade/src/app/arthurteng/page.tsx
@@ -1,14 +1,36 @@
 "use client";
 export default function page() {
-  const playJakingit = () => {
-    const audio = new Audio("/audio/im-jaking-it-made-with-Voicemod.mp3");
-    audio.loop = true;
-    void audio.play();
-  };
+  // const playJakingit = () => {
+  //   const audio = new Audio("/audio/im-jaking-it-made-with-Voicemod.mp3");
+  //   audio.loop = true;
+  //   void audio.play();
+  // };
   return (
-    <div id="content-container" className="bg-blue-300">
-      <div id="content">
-        <div id="nav-bar-container"></div>
+    <div
+      id="content-container"
+      className="absolute flex h-screen w-screen justify-center bg-gray-700"
+    >
+      <div id="content" className="w-[1900px]">
+        <div id="navbar-container" className="mt-4">
+          <div
+            id="navbar"
+            className="flex h-28 items-center justify-evenly rounded-sm bg-gray-800 text-lg"
+          >
+            <a href="/">Home</a>
+            <p>|</p>
+            <a href="/">About</a>
+            <p>|</p>
+            <a href="/">Contact</a>
+            <p>|</p>
+            <a href="/">Github</a>
+            <p>|</p>
+            <a href="/">LinkedIn</a>
+            <p>|</p>
+            <a href="/">Resume</a>
+            <p>|</p>
+            <a href="/">Website</a>
+          </div>
+        </div>
         <div id="main-content-container">
           <div id="left-ad-container"></div>
           <div id="essay-container"></div>
