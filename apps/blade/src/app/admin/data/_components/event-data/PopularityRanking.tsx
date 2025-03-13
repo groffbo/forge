a69f10e5364eb90e3ccd76f4e4cb49e58f408f9e
anna-sage-local <anna.macinnis109@gmail.com>
2025-03-13 18:45:05 -0400

    add show more toggle to top ten list

--- apps/blade/src/app/admin/data/_components/event-data/PopularityRanking.tsx
+++ apps/blade/src/app/admin/data/_components/event-data/PopularityRanking.tsx
@@ -1,7 +1,11 @@
 import type { ReturnEvent } from "@forge/db/schemas/knight-hacks";
+import { Button } from "@forge/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@forge/ui/card";
+import { useState } from "react";
 
 export default function PopularityRanking({ events } : { events: ReturnEvent[]; }) {
+    const [displayFullList, setDisplayFullList] = useState<boolean>(false);
+
     const topEvents = events.reduce<ReturnEvent[]>((heap, event) => {
         heap.push(event);
         heap.sort((a, b) => a.numAttended - b.numAttended);
@@ -18,19 +22,36 @@ export default function PopularityRanking({ events } : { events: ReturnEvent[]; 
         "text-lg font-medium text-orange-500", // bronze
     ];
 
+    const topThree = topEvents.slice(0, 3);
+
+    const handleClick = () => setDisplayFullList((prev) => !prev);
+
     return (
         <Card className="md:col-span-2 lg:col-span-2">
             <CardHeader>
                 <CardTitle className="text-xl">Most Popular Events</CardTitle>
             </CardHeader>
             <CardContent>
-                <ol className="flex flex-col gap-2">
-                    {topEvents.map((event, index) => (
-                        <li key={event.id} className={(rankingStyles[index] ?? "text-gray-400") + " flex justify-between"}>
-                            <span>{index+1}. {event.name}</span><span>{event.numAttended} attendees</span>
-                        </li>
-                    ))}
+                <ol className="flex flex-col gap-2 mb-4">
+                    { displayFullList ?
+                        topEvents.map((event, index) => (
+                            <li key={event.id} className={(rankingStyles[index] ?? "text-gray-400") + " flex justify-between"}>
+                                <span>{index+1}. {event.name}</span><span>{event.numAttended} attendees</span>
+                            </li>
+                        ))
+                        :
+                        topThree.map((event, index) => (
+                            <li key={event.id} className={(rankingStyles[index] ?? "text-gray-400") + " flex justify-between"}>
+                                <span>{index+1}. {event.name}</span><span>{event.numAttended} attendees</span>
+                            </li>
+                        ))
+                    }
                 </ol>
+                <div className="flex justify-center">
+                    <Button variant={"secondary"} onClick={handleClick}>
+                        {displayFullList ? "Show less" : "Show more"}
+                    </Button>
+                </div>
             </CardContent>
         </Card>
     );
