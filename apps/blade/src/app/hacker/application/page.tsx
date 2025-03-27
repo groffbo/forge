1f9739ec890ce36d4736b8b4ad78f8c1db30f7e1
unknown <danielram2023@gmail.com>
2025-03-27 18:06:49 -0400

    fix for linting

--- apps/blade/src/app/hacker/application/page.tsx
+++ apps/blade/src/app/hacker/application/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
 import { z } from "zod";
