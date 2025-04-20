bd2928368e3a3cb6d942f6be690c6171a78f29d6
Arthur <arthurteng0819@gmail.com>
2025-04-19 23:36:55 -0400

    added placeholder containers for left and right background svgs and changed z position of placeholder and contacctform containers

--- apps/club/src/app/contact/page.tsx
+++ apps/club/src/app/contact/page.tsx
@@ -9,18 +9,18 @@ export default function page() {
       id="contactPage"
       className="font-narrow flex h-screen flex-row items-center justify-center bg-orange-400 text-white"
     >
-      {/* <div
+      <div
         id="leftSide"
         className="fixed left-0 z-0 h-screen w-1/6 min-w-64 bg-black"
-      ></div> */}
+      ></div>
       <div id="middleContainer" className="z-50">
         <Header />
         <ContactForm />
       </div>
-      {/* <div
+      <div
         id="rightSide"
         className="fixed right-0 z-0 h-screen w-1/6 min-w-64 bg-black"
-      ></div> */}
+      ></div>
     </div>
   );
 }
