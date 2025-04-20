432aeed4a179b73d4f3f4c8efeef00dc7ecb57e8
Arthur <arthurteng0819@gmail.com>
2025-04-19 23:34:30 -0400

    add labels and inputs to the div structure, changed some names in the div

--- apps/club/src/app/contact/_components/contact-form.tsx
+++ apps/club/src/app/contact/_components/contact-form.tsx
@@ -1,25 +1,38 @@
 "use client";
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 function ContactForm() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  //   email: "",
+  //   subject: "",
+  //   message: "",
+  // });
   return (
     <div
       id="contactform"
-      className="text-bold w-auto bg-white font-bold text-black"
+      className="text-bold mt-2 w-auto bg-white p-5 font-bold text-black"
     >
       <form action="" className="space-y-4">
-        <div id="Name">Name</div>
-        <div id="Email">Email</div>
-        <div id="Subject">Subject</div>
-        <div id="Message">Message</div>
-        <div id="contactOption">Options</div>
+        <div id="Name">
+          <label className="mb-2 block">Name</label>
+          <input type="text" name="name" className="border" />
+        </div>
+        <div id="Email">
+          <label className="mb-2 block">Email</label>
+          <input type="text" name="name" className="border" />
+        </div>
+        <div id="Subject">
+          <label className="mb-2 block">Message Subject</label>
+          <input type="text" name="name" className="border" />
+        </div>
+        <div id="Message">
+          <label className="mb-2 block">Message Body</label>
+          <input type="text" name="name" className="border" />
+        </div>
+        <div id="contactOption">Which team would you like to contact?</div>
+
         <button>Contact Us</button>
       </form>
     </div>
--- apps/club/src/app/contact/page.tsx
+++ apps/club/src/app/contact/page.tsx
@@ -7,12 +7,20 @@ export default function page() {
   return (
     <div
       id="contactPage"
-      className="font-narrow flex h-screen flex-col items-center justify-center bg-orange-400 text-white"
+      className="font-narrow flex h-screen flex-row items-center justify-center bg-orange-400 text-white"
     >
-      <div id="mainContentContainer">
+      {/* <div
+        id="leftSide"
+        className="fixed left-0 z-0 h-screen w-1/6 min-w-64 bg-black"
+      ></div> */}
+      <div id="middleContainer" className="z-50">
         <Header />
         <ContactForm />
       </div>
+      {/* <div
+        id="rightSide"
+        className="fixed right-0 z-0 h-screen w-1/6 min-w-64 bg-black"
+      ></div> */}
     </div>
   );
 }
