bdba61f6a0a971c81f84d0e266bf3856917f67d6
Daniel Palma <71732131+DanyPalma@users.noreply.github.com>
2025-04-22 03:22:05 -0400

    Update webring-members.ts

--- packages/consts/src/webring-members.ts
+++ packages/consts/src/webring-members.ts
@@ -1,11 +1,11 @@
 export const WEBRING_MEMBERS = {
-    "sites": [
-        {
-            "name": "Daniel Palma",
-            "year": "2026",
-            "website": "https://dpalma.dev",
-            "image": "/dany.jpg",
-            "role": "Dev lead 2024",
-        },
-    ]
+  sites: [
+    {
+      name: "Daniel Palma",
+      year: "2026",
+      website: "https://dpalma.dev",
+      image: "/dany.jpg",
+      role: "Dev lead 2024",
+    },
+  ],
 } as const;
