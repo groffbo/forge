4d3816a4a5374fe19c49fb93b6db84f288d7cec5
Arthur <arthurteng0819@gmail.com>
2025-05-09 18:29:17 -0400

    modfied css to move around background react components  to look nice on tablets; tablet designs are completely finished

--- apps/club/src/app/contact/_components/left-side.tsx
+++ apps/club/src/app/contact/_components/left-side.tsx
@@ -7,10 +7,10 @@ import SwordLeft from "./assets/sword-left";
 function LeftSide() {
   return (
     <div className="fixed left-0 z-0 h-full w-1/2 overflow-hidden">
-      <div className="bg-contact-gradient flex h-full flex-col space-y-11">
-        <SwordLeft className="mt-[100px] w-[80%] -translate-x-[2%] transform" />
-        <AbstractLeft className="relative w-[80%] -translate-x-[47%] transform" />
-        <Shield className="w-[80%]" />
+      <div className="bg-contact-gradient flex h-full flex-col space-y-11 md:space-y-28 lg:space-y-28">
+        <SwordLeft className="mt-[100px] w-[150px] -translate-x-[2%] transform md:w-[230px] lg:w-[300px]" />
+        <AbstractLeft className="relative w-[150px] -translate-x-[47%] transform md:w-[230px] lg:w-[300px]" />
+        <Shield className="w-[150px] md:w-[230px] lg:w-[300px] xl:w-[270px]" />
       </div>
     </div>
   );
--- apps/club/src/app/contact/_components/right-side.tsx
+++ apps/club/src/app/contact/_components/right-side.tsx
@@ -7,10 +7,10 @@ import SwordRight from "./assets/sword-left";
 function RightSide() {
   return (
     <div className="fixed right-0 z-0 h-full w-1/2 scale-x-[-1]">
-      <div className="bg-contact-gradient flex h-full flex-col space-y-11">
-        <SwordRight className="mt-[100px] w-[80%] -translate-x-[2%] transform" />
-        <AbstractRight className="relative w-[80%] -translate-x-[47%] transform" />
-        <Shield className="left-0 w-[80%]" />
+      <div className="bg-contact-gradient flex h-full flex-col space-y-11 md:space-y-28 lg:space-y-28">
+        <SwordRight className="mt-[100px] w-[150px] -translate-x-[2%] transform md:w-[230px] lg:w-[300px]" />
+        <AbstractRight className="relative w-[150px] -translate-x-[47%] transform md:w-[230px] lg:w-[300px]" />
+        <Shield className="left-0 w-[150px] md:w-[230px] lg:w-[300px]" />
       </div>
     </div>
   );
