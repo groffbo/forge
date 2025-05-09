70278152ca151d715cd0882a83776a35507a38a1
must108 <mustaeen18@gmail.com>
2025-05-08 21:36:59 -0400

    fixed hacker delete bug

--- packages/api/src/routers/hacker.ts
+++ packages/api/src/routers/hacker.ts
@@ -14,6 +14,7 @@ import {
 
 import { adminProcedure, protectedProcedure } from "../trpc";
 import { log } from "../utils";
+import { Session } from "@forge/db/schemas/auth";
 
 export const hackerRouter = {
   getHacker: protectedProcedure.query(async ({ ctx }) => {
