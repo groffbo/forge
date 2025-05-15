819520176952864eed5f6131f63301e580111032
must108 <mustaeen18@gmail.com>
2025-05-15 11:18:52 -0400

    admin dashboard restructuring

--- apps/blade/src/app/hacker/application/page.tsx
+++ apps/blade/src/app/hacker/application/page.tsx
@@ -9,24 +9,24 @@ import { SIGN_IN_PATH } from "~/consts";
 // import { api } from "~/trpc/server";
 // import { HackerFormPage } from "./_components/hacker-application-form";
 
-// export default async function HackerApplicationPage() {
-export default function HackerApplicationPage() {
-  redirect(SIGN_IN_PATH);
-  // const session = await auth();
-
-  // if (session == null) {
+export default async function HackerApplicationPage() {
+  // export default function HackerApplicationPage() {
   //   redirect(SIGN_IN_PATH);
-  // }
+  const session = await auth();
 
-  // const isHacker = await api.hacker.getHacker();
+  if (session == null) {
+    redirect(SIGN_IN_PATH);
+  }
 
-  // if (isHacker) {
-  //   return redirect(SIGN_IN_PATH);
-  // }
+  const isHacker = await api.hacker.getHacker();
 
-  // return (
-  //   <main className="px-8 py-4">
-  //     <HackerFormPage />
-  //   </main>
-  // );
+  if (isHacker) {
+    return redirect(SIGN_IN_PATH);
+  }
+
+  return (
+    <main className="px-8 py-4">
+      <HackerFormPage />
+    </main>
+  );
 }
