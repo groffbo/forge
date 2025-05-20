601b78423db3836a0948ede64bf63c2efb313c63
must108 <mustaeen18@gmail.com>
2025-05-20 03:11:51 -0400

    a few changes

--- apps/blade/src/app/hacker/application/page.tsx
+++ apps/blade/src/app/hacker/application/page.tsx
@@ -25,21 +25,17 @@ export default function HackerApplicationPage() {
 
   // if (session == null) {
   //   redirect(SIGN_IN_PATH);
-  const session = await auth();
+  // }
 
-  if (session == null) {
-    redirect(SIGN_IN_PATH);
-  }
+  // const isHacker = await api.hacker.getHacker();
 
-  const isHacker = await api.hacker.getHacker();
+  // if (isHacker) {
+  //   return redirect(SIGN_IN_PATH);
+  // }
 
-  if (isHacker) {
-    return redirect(SIGN_IN_PATH);
-  }
-
-  return (
-    <main className="px-8 py-4">
-      <HackerFormPage />
-    </main>
-  );
+  // return (
+  //   <main className="px-8 py-4">
+  //     <HackerFormPage />
+  //   </main>
+  // );
 }
