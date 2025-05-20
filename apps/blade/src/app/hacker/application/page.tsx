3859f22fb700e7aa82920eb7f4d04f0db78032b6
must108 <mustaeen18@gmail.com>
2025-05-20 03:11:51 -0400

    a few changes

--- apps/blade/src/app/hacker/application/page.tsx
+++ apps/blade/src/app/hacker/application/page.tsx
@@ -5,28 +5,27 @@ import { redirect } from "next/navigation";
 // import { auth } from "@forge/auth";
 
 import { SIGN_IN_PATH } from "~/consts";
-
 // import { api } from "~/trpc/server";
 // import { HackerFormPage } from "./_components/hacker-application-form";
 
-export default async function HackerApplicationPage() {
-  // export default function HackerApplicationPage() {
-  //   redirect(SIGN_IN_PATH);
-  const session = await auth();
-
-  if (session == null) {
+// export default async function HackerApplicationPage() {
+export default function HackerApplicationPage() {
     redirect(SIGN_IN_PATH);
-  }
+  // const session = await auth();
 
-  const isHacker = await api.hacker.getHacker();
+  // if (session == null) {
+  //   redirect(SIGN_IN_PATH);
+  // }
 
-  if (isHacker) {
-    return redirect(SIGN_IN_PATH);
-  }
+  // const isHacker = await api.hacker.getHacker();
 
-  return (
-    <main className="px-8 py-4">
-      <HackerFormPage />
-    </main>
-  );
+  // if (isHacker) {
+  //   return redirect(SIGN_IN_PATH);
+  // }
+
+  // return (
+  //   <main className="px-8 py-4">
+  //     <HackerFormPage />
+  //   </main>
+  // );
 }
--- packages/db/src/schemas/knight-hacks.ts
+++ packages/db/src/schemas/knight-hacks.ts
@@ -107,12 +107,9 @@ export const Hacker = createTable(
     resumeUrl: t.varchar({ length: 255 }),
     dob: t.date().notNull(),
     gradDate: t.date().notNull(),
-    status: t
-      .text("status", {
-        enum: HACKATHON_APPLICATION_STATES,
-      })
-      .notNull()
-      .default("pending"),
+    status: t.text("status", {
+      enum: HACKATHON_APPLICATION_STATES,
+    }).notNull().default("pending"),
     survey1: t.text("survey_1").notNull(),
     survey2: t.text("survey_2").notNull(),
     isFirstTime: t.boolean("is_first_time").default(false),
