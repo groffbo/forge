17711a6a792aea1610aed02ce001912a237f1609
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-05-20 02:04:40 -0400

    Fix package error

--- apps/2025/src/app/layout.tsx
+++ apps/2025/src/app/layout.tsx
@@ -1,24 +1,18 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Knight Hacks 2025",
-  description: "Orlando's largest hackathon",
+  description: "The largest hackathon in Orlando",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
-
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
