73d3d9daca7488592b155c65785a19575be8118c
must108 <mustaeen18@gmail.com>
2025-05-20 03:11:51 -0400

    a few changes

--- apps/blade/src/app/hacker/application/page.tsx
+++ apps/blade/src/app/hacker/application/page.tsx
@@ -3,9 +3,12 @@ import { redirect } from "next/navigation";
 // Uncomment if you're a dev and need to use the hacker application
 
 // import { auth } from "@forge/auth";
+// import { auth } from "@forge/auth";
 
 import { SIGN_IN_PATH } from "~/consts";
 
+// import { api } from "~/trpc/server";
+// import { HackerFormPage } from "./_components/hacker-application-form";
 // import { api } from "~/trpc/server";
 // import { HackerFormPage } from "./_components/hacker-application-form";
 
@@ -15,22 +18,24 @@ import { SIGN_IN_PATH } from "~/consts";
   // const session = await auth();
 
   // if (session == null) {
-  //   redirect(SIGN_IN_PATH);
-  const session = await auth();
-
-  if (session == null) {
+// export default async function HackerApplicationPage() {
+export default function HackerApplicationPage() {
     redirect(SIGN_IN_PATH);
-  }
+  // const session = await auth();
 
-  const isHacker = await api.hacker.getHacker();
+  // if (session == null) {
+  //   redirect(SIGN_IN_PATH);
+  // }
 
-  if (isHacker) {
-    return redirect(SIGN_IN_PATH);
-  }
+  // const isHacker = await api.hacker.getHacker();
 
-  return (
-    <main className="px-8 py-4">
-      <HackerFormPage />
-    </main>
-  );
+  // if (isHacker) {
+  //   return redirect(SIGN_IN_PATH);
+  // }
+
+  // return (
+  //   <main className="px-8 py-4">
+  //     <HackerFormPage />
+  //   </main>
+  // );
 }
