3190c2b453bb78e586c3a3e21539160fd958d8cd
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-05-23 19:16:01 -0400

    Fix next config

--- apps/2025/next.config.js
+++ apps/2025/next.config.js
@@ -1,10 +1,25 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.ts";
+import { fileURLToPath } from "url";
+import { createJiti } from "jiti";
+
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+await createJiti(fileURLToPath(import.meta.url)).import("./src/env");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  reactStrictMode: true,
+
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+    "@forge/api",
+    "@forge/auth",
+    "@forge/db",
+    "@forge/ui",
+    "@forge/validators",
+  ],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
 
 export default config;
