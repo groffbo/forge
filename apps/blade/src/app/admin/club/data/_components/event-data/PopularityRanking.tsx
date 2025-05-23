fc13de418a494653c7992faf3b6394428be430dd
anna-sage-local <anna.macinnis109@gmail.com>
2025-05-23 10:05:46 -0400

    display msg when no attendance data exists for popularity ranking

--- apps/blade/src/app/admin/club/data/_components/event-data/PopularityRanking.tsx
+++ apps/blade/src/app/admin/club/data/_components/event-data/PopularityRanking.tsx
@@ -25,21 +25,26 @@ export default function PopularityRanking({
         <CardTitle className="text-xl">Most Popular Events</CardTitle>
       </CardHeader>
       <CardContent>
-        <ol className="mb-4 flex flex-col gap-2">
-          {(displayFullList ? topEvents : topEvents.slice(0, 3)).map(
-            (event, index: number) => (
-              <li
-                key={event.id}
-                className={`flex justify-between text-sm ${RANKING_STYLES[index] ?? "text-gray-400 md:text-base lg:text-base"}`}
-              >
-                <span className="me-4">
-                  {index + 1}. {event.name} &#91;{event.tag.toUpperCase()}&#93;
-                </span>
-                <span>{event.numAttended} attended</span>
-              </li>
-            ),
-          )}
-        </ol>
+        {
+          topEvents.length > 0 ?
+          <ol className="mb-4 flex flex-col gap-2">
+            {(displayFullList ? topEvents : topEvents.slice(0, 3)).map(
+              (event, index: number) => (
+                <li
+                  key={event.id}
+                  className={`flex justify-between text-sm ${RANKING_STYLES[index] ?? "text-gray-400 md:text-base lg:text-base"}`}
+                >
+                  <span className="me-4">
+                    {index + 1}. {event.name} &#91;{event.tag.toUpperCase()}&#93;
+                  </span>
+                  <span>{event.numAttended} attended</span>
+                </li>
+              ),
+            )}
+          </ol>
+          :
+          <p className="mt-16 mb-20 text-center text-slate-300">No attendance data found</p>
+        }
         <div className="flex justify-center">
         {
           topEvents.length > 3 && // no need for show more toggle if there are 3 or less events
