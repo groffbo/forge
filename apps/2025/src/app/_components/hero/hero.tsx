86f544e823d2b7a5561c286fb3efc46dfeb25345
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-05-23 21:06:37 -0400

    Add button

--- apps/2025/src/app/_components/hero/hero.tsx
+++ apps/2025/src/app/_components/hero/hero.tsx
@@ -1,11 +1,19 @@
 import HeroSVG from "./hero-svg";
 import RegisterButton from "./register-button";
+import { env } from "~/env";
 
 export default function Hero() {
   return (
     <div className="flex flex-col items-center space-y-6">
       <HeroSVG className="h-auto w-1/2 md:w-1/4" />
-      <RegisterButton className="h-auto w-1/2 md:w-1/4" />
+      <a
+        href={`${env.BLADE_URL}/hacker/application`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex justify-center"
+      >
+        <RegisterButton className="h-auto w-1/2 md:w-1/4" />
+      </a>
     </div>
   );
 }
