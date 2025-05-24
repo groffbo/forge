02ede1775f9d8f065590b0d3eba1ceecf832613e
Sam <samuelxborges@gmail.com>
2025-05-24 06:42:57 -0400

    shadow pmo

--- apps/2025/src/app/_components/footer/footer.tsx
+++ apps/2025/src/app/_components/footer/footer.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Bebas_Neue } from "next/font/google"; 
 
 const footerSVG = "./khFull.svg";
+const shadow = "./shadow.svg";
 const mlhcoc = "https://mlh.io/code-of-conduct";
 const sponsor = "https://blade.knighthacks.org/sponsor";
 const contact = "mailto:hack@knighthacks.org";
@@ -15,12 +16,13 @@ const bn = Bebas_Neue({
 
 export default function Footer() {
   return (
-    <div className="flex flex-col items-center space-y-6 mx-auto mt-10">
-        <div className="flex w-1/3 justify-center">
-            <Image src={footerSVG} alt="Knight Hacks" width={2267} height={803} />
+    <div className="flex flex-col items-center mx-auto">
+        <div className="relative w-full">
+            <Image src={shadow} alt="Shadow" width={2000} height={2000} className="w-full opacity-75" />
+            <Image src={footerSVG} alt="Knight Hacks" width={2267} height={803} className="absolute bottom-0 left-1/2 -translate-x-1/2 w-1/3 mb-20" />
         </div>
-        <div className={`${bn.className} flex flex-col w-full justify-center bg-black text-white py-15`}>
-            <div className="flex justify-center text-center mb-3 px-2 text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+        <div className={`${bn.className} flex flex-col w-full justify-center bg-black text-white py-15 -mt-10`}>
+            <div className="flex justify-center text-center mb-3 px-2 text-lg sm:text-xl md:text-3xl lg:text-4xl">
                 <Link href={mlhcoc} className="flex transform transition px-3 hover:scale-105 hover:underline">MLH code of conduct </Link>
                  | 
                 <Link href={sponsor} className="flex transform transition px-3 hover:scale-105 hover:underline"> sponsor us </Link>
