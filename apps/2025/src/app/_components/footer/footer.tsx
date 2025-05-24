ee6c789daf90d5103f331a767a899bccb28889aa
Sam <samuelxborges@gmail.com>
2025-05-24 05:46:43 -0400

    this is what it will look like

--- apps/2025/src/app/_components/footer/footer.tsx
+++ apps/2025/src/app/_components/footer/footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Bebas_Neue } from "next/font/google"; 
+import { Bebas_Neue } from "next/font/google";
 
 const footerSVG = "./khFull.svg";
 const shadow = "./shadow.svg";
@@ -16,28 +16,68 @@ const bn = Bebas_Neue({
 
 export default function Footer() {
   return (
-    <div className="flex flex-col items-center mx-auto">
-        <div className="relative w-full">
-            <Image src={shadow} alt="Shadow" width={0} height={0} sizes="100vw" objectFit="cover" className="absolute w-full opacity-75 bottom-0 left-1/2 -translate-x-1/2 mb-10" />
-            <Image src={footerSVG} alt="Knight Hacks" width={2267} height={803} objectFit="cover" className="absolute bottom-0 left-1/2 -translate-x-1/2 w-1/3 mb-20" />
+    <div className="mx-auto flex flex-col items-center">
+      <div className="relative w-full">
+        <Image
+          src={shadow}
+          alt="Shadow"
+          width={0}
+          height={0}
+          sizes="100vw"
+          objectFit="cover"
+          className="absolute bottom-0 left-1/2 mb-10 w-full -translate-x-1/2 opacity-75"
+        />
+        <Image
+          src={footerSVG}
+          alt="Knight Hacks"
+          width={2267}
+          height={803}
+          objectFit="cover"
+          className="absolute bottom-0 left-1/2 mb-20 w-1/3 -translate-x-1/2"
+        />
+      </div>
+      <div
+        className={`${bn.className} -mt-10 flex w-full flex-col justify-center bg-black pt-15 pb-5 text-white`}
+      >
+        <div className="mb-3 flex justify-center px-2 text-center text-lg sm:text-xl md:text-3xl lg:text-4xl">
+          <Link
+            href={mlhcoc}
+            className="flex transform px-3 transition hover:scale-105 hover:underline"
+          >
+            MLH code of conduct{" "}
+          </Link>
+          |
+          <Link
+            href={sponsor}
+            className="flex transform px-3 transition hover:scale-105 hover:underline"
+          >
+            {" "}
+            sponsor us{" "}
+          </Link>
+          |
+          <Link
+            href={contact}
+            className="flex transform px-3 transition hover:scale-105 hover:underline"
+          >
+            {" "}
+            contact us{" "}
+          </Link>
+          |
+          <Link
+            href={hackersGuide}
+            className="flex transform px-3 transition hover:scale-105 hover:underline"
+          >
+            {" "}
+            hackers guide
+          </Link>
         </div>
-        <div className={`${bn.className} flex flex-col w-full justify-center bg-black text-white pt-15 pb-5 -mt-10`}>
-            <div className="flex justify-center text-center mb-3 px-2 text-lg sm:text-xl md:text-3xl lg:text-4xl">
-                <Link href={mlhcoc} className="flex transform transition px-3 hover:scale-105 hover:underline">MLH code of conduct </Link>
-                 | 
-                <Link href={sponsor} className="flex transform transition px-3 hover:scale-105 hover:underline"> sponsor us </Link>
-                 | 
-                <Link href={contact} className="flex transform transition px-3 hover:scale-105 hover:underline"> contact us </Link>
-                 | 
-                <Link href={hackersGuide} className="flex transform transition px-3 hover:scale-105 hover:underline"> hackers guide</Link>
-            </div>
-            <span className="pb-10 justify-center text-center text-base sm:text-lg md:text-xl lg:text-2xl">
-                Copyright © 2019 - 2025 knighthacks. All Rights Reserved.
-            </span>
-            <span className="justify-center text-center text-sm sm:text-md md:text-lg lg:text-xl">
-                Made with 💜 by the Knight Hacks team. 
-            </span>
-        </div>
+        <span className="justify-center pb-10 text-center text-base sm:text-lg md:text-xl lg:text-2xl">
+          Copyright © 2019 - 2025 knighthacks. All Rights Reserved.
+        </span>
+        <span className="sm:text-md justify-center text-center text-sm md:text-lg lg:text-xl">
+          Made with 💜 by the Knight Hacks team.
+        </span>
+      </div>
     </div>
   );
 }
