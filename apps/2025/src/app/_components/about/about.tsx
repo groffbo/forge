0afd5686a6e7475ea88af5e23d9fe428bbd6ffc7
unknown <danielram2023@gmail.com>
2025-05-24 12:19:36 -0400

    adjust size

--- apps/2025/src/app/_components/about/about.tsx
+++ apps/2025/src/app/_components/about/about.tsx
@@ -5,7 +5,7 @@ import GraphicSVG from "./graphic-svg";
 export default function Hero() {
   return (
     <div className="w-full flex justify-center px-4">
-      <div className="relative w-[99%] md:w-[50%]">
+      <div className="relative w-[99%] md:w-[60%]">
         <AboutSVG className="w-full h-auto my-[10%]" />
         <GraphicSVG />
 
