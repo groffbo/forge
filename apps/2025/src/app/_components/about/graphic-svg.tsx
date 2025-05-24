4f43c848417dac38d8176a64f19ce8a60d212b71
unknown <danielram2023@gmail.com>
2025-05-24 11:57:48 -0400

    add about components

---
+++ apps/2025/src/app/_components/about/graphic-svg.tsx
@@ -0,0 +1,12 @@
+const GraphicSVG = () => {
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src="about-graphic.svg"
+      className="h-auto w-full"
+      draggable="false"
+    />
+  );
+};
+
+export default GraphicSVG;
