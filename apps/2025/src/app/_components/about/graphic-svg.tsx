09f98b2a6d7e4b2a700e6f1b94938d43b40556d1
unknown <danielram2023@gmail.com>
2025-05-25 01:42:29 -0400

    fix comic svg

--- apps/2025/src/app/_components/about/graphic-svg.tsx
+++
@@ -1,8 +0,0 @@
-const GraphicSVG = () => {
-  return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img src="about-graphic.svg" className="h-auto w-full" draggable="false" />
-  );
-};
-
-export default GraphicSVG;
