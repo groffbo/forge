1d10b9feff961a0dcdf5d1d9977caec4acf3fe48
Daniel Efres <da947619@ucf.edu>
2025-05-25 02:14:23 -0400

    sponsors

--- apps/club/src/app/_components/landing/sponsors.tsx
+++ apps/club/src/app/_components/landing/sponsors.tsx
@@ -7,16 +7,29 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import SponsorCard from "./sponsors-assets/sponsor-card";
 
 const companies = [
-  { name: "BNY MELLON", logo: "/logos/bny-mellon.svg" },
+  { name: "American Express", logo: "/logos/amex.svg" },
+  { name: "AWS", logo: "/logos/aws.svg" },
+  { name: "BNY Mellon", logo: "/logos/bny.svg" },
+  { name: "Capital One", logo: "/logos/c1.svg" },
   { name: "EA", logo: "/logos/ea.svg" },
   { name: "Facebook", logo: "/logos/facebook.svg" },
-  { name: "Geico", logo: "/logos/geico.svg" },
-  { name: "Google Cloud", logo: "/logos/google-cloud.svg" },
-  { name: "IBM", logo: "/logos/ibm.svg" },
-  { name: "Lockheed Martin", logo: "/logos/lockheed-martin.svg" },
+  { name: "Geico", logo: "/logos/Geico.svg" },
+  { name: "GitHub", logo: "/logos/github.svg" },
+  { name: "Goldman Sachs", logo: "/logos/goldman.svg" },
+  { name: "Google Cloud", logo: "/logos/googlecloud.svg" },
+  { name: "IBM", logo: "/logos/IBM.svg" },
+  { name: "JPMorgan", logo: "/logos/chase.svg" },
+  { name: "Lockheed Martin", logo: "/logos/lockheed.svg" },
   { name: "Microsoft", logo: "/logos/microsoft.svg" },
-  { name: "Synopsys", logo: "/logos/synopsys.svg" },
+  { name: "NASA", logo: "/logos/nasa.svg" },
+  { name: "NextEra", logo: "/logos/nextera.svg" },
+  { name: "RBC", logo: "/logos/rbc.svg" },
+  { name: "ServiceNow", logo: "/logos/ServiceNow.svg" },
+  { name: "Synopsys", logo: "/logos/Synopsys.svg" },
+  { name: "Texas Instruments", logo: "/logos/texasinstruments.svg" },
 ];
+
+
 
 export default function Sponsors() {
   gsap.registerPlugin(ScrollTrigger);
@@ -32,7 +45,7 @@ export default function Sponsors() {
     >
       <h2
         ref={headerRef}
-        className="font-pragati pb-16 md:pb-32 text-2xl font-bold leading-tight tracking-tight text-white [text-shadow:0px_0px_40px_#6B21A8,0px_0px_20px_#6B21A8,0px_0px_10px_#6B21A8] md:text-5xl lg:text-6xl"
+        className="font-pragati pt-32 pb-24 md:pt-12 md:pb-32 text-2xl font-bold leading-tight tracking-tight text-white [text-shadow:0px_0px_40px_#6B21A8,0px_0px_20px_#6B21A8,0px_0px_10px_#6B21A8] md:text-5xl lg:text-6xl"
       >
         Previously Sponsored By
       </h2>
@@ -40,11 +53,12 @@ export default function Sponsors() {
         ref={sponsorContainerRef}
         className="flex flex-wrap justify-center gap-6 px-4"
       >
-        {companies.map((company) => (
+        {companies.map((company, index) => (
           <SponsorCard
             key={company.name}
             sponsor={company.name}
             imgUrl={company.logo}
+            index={index}
           />
         ))}
       </div>
