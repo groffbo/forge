c8ea7d7fa6c3d88737e282da907abb5d1e7ffd11
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-05-24 22:51:40 -0400

    fix hero screen and padding

--- apps/2025/src/app/_components/hero/hero.tsx
+++ apps/2025/src/app/_components/hero/hero.tsx
@@ -5,9 +5,7 @@ export default function Hero() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center space-y-6">
       <HeroSVG className="h-auto w-1/2 md:w-1/3" />
-      
-        <RegisterButton />
-      
+      <RegisterButton />
     </div>
   );
 }
