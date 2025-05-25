0eb6e9ec9dbb28575b44ba0cd6c0feba8657a12b
unknown <danielram2023@gmail.com>
2025-05-25 01:42:29 -0400

    fix comic svg

--- apps/2025/src/app/_components/about/about.tsx
+++ apps/2025/src/app/_components/about/about.tsx
@@ -1,12 +1,12 @@
 import AboutSVG from "./about-svg";
-import GraphicSVG from "./graphic-svg";
+import AboutComicSVG from "./about-comic-svg";
 
 export default function Hero() {
   return (
     <div className="flex w-full justify-center px-4">
       <div className="relative w-[99%] md:w-[60%]">
         <AboutSVG className="my-[10%] h-auto w-full" />
-        <GraphicSVG />
+        <AboutComicSVG className="my-[10%] h-auto w-full" />
       </div>
     </div>
   );
