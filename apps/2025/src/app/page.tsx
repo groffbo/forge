6573e0e23dcafaa7212030057b350a1dcc687bc2
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-05-24 22:52:47 -0400

    Fix body css

--- apps/2025/src/app/page.tsx
+++ apps/2025/src/app/page.tsx
@@ -3,7 +3,7 @@ import Tracks from "./_components/tracks/tracks";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center bg-[url('/background.svg')] bg-cover pt-20">
+    <div className="bg-[url('/background.svg')] bg-cover pt-20">
       <Hero />
       <Tracks />
     </div>
