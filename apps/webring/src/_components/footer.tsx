11352923bc83e26a7575b4c3ec7cee32635506d1
Dylan Vidal <dylanvidal1204@gmail.com>
2025-05-25 18:03:53 -0700

    init guild + port conflicts

--- apps/webring/src/_components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center bg-black text-white">
-      ENTER THE COLLECTIVE
-    </div>
-  );
-}
