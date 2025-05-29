84dea5cbbd87a987e77b23aa7444b477c5d3b0cd
Daniel Palma <71732131+DanyPalma@users.noreply.github.com>
2025-05-29 10:29:26 -0400

    Update page.tsx

--- apps/2025/src/app/page.tsx
+++ apps/2025/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "./_components/hero/hero";
 import Tracks from "./_components/tracks/tracks";
 import About from "./_components/about/about";
+import Faq from "./_components/faq/faq";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Tracks />
+      <Faq />
     </div>
   );
 }
