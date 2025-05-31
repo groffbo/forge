a1b23e78428f1fe872aa995a6f878cde03a869ad
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-05-30 21:12:44 -0400

    remove console log

--- apps/blade/src/app/dashboard/_components/member-dashboard/member-dashboard.tsx
+++ apps/blade/src/app/dashboard/_components/member-dashboard/member-dashboard.tsx
@@ -36,8 +36,6 @@ export default async function MemberDashboard({
     );
   }
 
-  console.log("Events: ", events.status);
-
   return (
     <div className="flex-col md:flex">
       <div className="flex-1 space-y-4 p-8 pt-6">
