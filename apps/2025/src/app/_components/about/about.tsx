0aa35ed742e0b968aaa99d90f476ee841af32a6c
Sam <samuelxborges@gmail.com>
2025-06-02 19:16:20 -0400

    made a new logo tsx for general use

--- apps/2025/src/app/_components/about/about.tsx
+++ apps/2025/src/app/_components/about/about.tsx
@@ -1,11 +1,12 @@
 import AboutSVG from "./about-svg";
 import AboutComicSVG from "./about-comic-svg";
+import Logo from "../logo";
 
 export default function About() {
   return (
-    <div className="flex w-full justify-center px-4 z-10">
+    <div className="z-10 flex w-full justify-center px-4">
       <div className="relative w-[99%] md:w-[60%]">
-        <AboutSVG className="mt-4 mb-[10%] md:my-[10%] h-auto w-full" />
+        <AboutSVG className="mt-4 mb-[10%] h-auto w-full md:my-[10%]" />
         <AboutComicSVG className="my-[10%] h-auto w-full" />
       </div>
     </div>
