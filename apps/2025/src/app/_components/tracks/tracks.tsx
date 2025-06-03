cd9f0a142cb63a44384f7f8bfa41affb5c803bb4
Sam <samuelxborges@gmail.com>
2025-06-03 01:46:37 -0400

    oops forgor

--- apps/2025/src/app/_components/tracks/tracks.tsx
+++ apps/2025/src/app/_components/tracks/tracks.tsx
@@ -1,13 +1,11 @@
 import Comic from "./comic";
-import TracksTitleSVG from "./tracks-title-svg";
 import TextBox from "../textbox";
 
 export default function Tracks() {
   return (
     <div className="my-[7%] flex flex-col items-center space-y-6 overflow-x-hidden z-10 mb-25">
       <div className="mb-0 w-full px-4 md:px-8">
-        <TracksTitleSVG className="h-auto w-full md:w-[50%]" />
-        <div className="flex items-start w-4/5">
+        <div className="flex items-start w-4/5 lg:w-2/3 mt-5 lg:ml-10">
           <TextBox width="100%" height="100%" textSize="text-sm sm:text-md md:text-xl lg:text-3xl" centering="text-start" className="mb-8 sm:mb-12 md:mb-16">
             <p>
               <em><strong>In battle, explore our tracks, such as</strong></em>
