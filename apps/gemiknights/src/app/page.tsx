1bd2f35a02c02a1d6bd18115c2ea9e6482426ea4
Sam <samuelxborges@gmail.com>
2025-06-09 04:43:26 -0400

    oops!

--- apps/gemiknights/src/app/page.tsx
+++ apps/gemiknights/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col justify-center w-full items-center text-4xl tk-forma-djr-display mt-20 pt-10">
       <Logo />
-      <div className="text-white">
+      <div className="text-white w-full">
         <About />
         <FAQ />
         <div className="flex gap-4 items-center w-full justify-center">
