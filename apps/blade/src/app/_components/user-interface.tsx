e51554a0c3e4022d77db2311bb6e2a177eca4905
Dylan Vidal <dylanvidal1204@gmail.com>
2025-07-05 14:10:13 -0700

    semantics

--- apps/blade/src/app/_components/user-interface.tsx
+++ apps/blade/src/app/_components/user-interface.tsx
@@ -23,8 +23,9 @@ export async function UserInterface() {
     return (
       <div className="flex flex-col items-center justify-center gap-y-6 font-bold">
         <p className="w-full max-w-xl text-center">
-          You have not applied to be a Knight Hacks member or hacker yet. Please
-          fill out an application below to Get Started!
+          You have not applied to be a Knight Hacks member or hacker for an
+          upcoming Hackathon yet. Please fill out an application below to get
+          started!
         </p>
         <div className="flex flex-wrap justify-center gap-5">
           <MemberAppCard />
