f71289996998e5770bdf3b55c7ae305892503d91
Dylan Vidal <dylanvidal1204@gmail.com>
2025-07-15 13:44:14 -0700

    hackers guide

--- apps/gemiknights/src/app/_components/faq/faq.tsx
+++ apps/gemiknights/src/app/_components/faq/faq.tsx
@@ -33,7 +33,7 @@ const FAQ = () => {
             <AccordionTrigger className="w-full rounded-lg px-4 py-2 text-left text-base font-semibold transition-all duration-500 hover:scale-[1.02] hover:bg-white/5 sm:px-5 sm:py-3 sm:text-lg md:px-6 md:py-4 md:text-xl">
               {item.question}
             </AccordionTrigger>
-            <AccordionContent className="tk-peridot-devanagari overflow-hidden p-4 text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down sm:p-5 sm:text-base md:p-6 md:text-lg">
+            <AccordionContent className="tk-peridot-devanagari overflow-hidden p-4 text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down sm:p-5 sm:text-lg md:p-6 md:text-xl">
               {item.answer}
             </AccordionContent>
           </AccordionItem>
