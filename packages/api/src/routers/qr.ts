958168bd22c1e671d2f637fe249a84ce4933d80a
Luke Cullen <75819270+retromonos@users.noreply.github.com>
2025-08-28 11:31:29 -0400

    Generate QR code on null code

--- packages/api/src/routers/qr.ts
+++ packages/api/src/routers/qr.ts
@@ -1,6 +1,10 @@
 import type { TRPCRouterRecord } from "@trpc/server";
+import QRCode from "qrcode";
 
-import { BUCKET_NAME } from "@forge/consts/knight-hacks";
+import {
+  BUCKET_NAME,
+  KNIGHTHACKS_S3_BUCKET_REGION,
+} from "@forge/consts/knight-hacks";
 
 import { minioClient } from "../minio/minio-client";
 import { protectedProcedure } from "../trpc";
@@ -11,11 +15,33 @@ export const qrRouter = {
     const objectName = `qr-code-${userId}.png`;
 
     try {
+      try {
+        await minioClient.statObject(BUCKET_NAME, objectName);
+      } catch {
+        const bucketExists = await minioClient.bucketExists(BUCKET_NAME);
+        if (!bucketExists) {
+          await minioClient.makeBucket(
+            BUCKET_NAME,
+            KNIGHTHACKS_S3_BUCKET_REGION,
+          );
+        }
+        const qrData = `user:${userId}`;
+        const qrBuffer = await QRCode.toBuffer(qrData, { type: "png" });
+        await minioClient.putObject(
+          BUCKET_NAME,
+          objectName,
+          qrBuffer,
+          qrBuffer.length,
+          { "Content-Type": "image/png" },
+        );
+      }
+
       const qrCodeUrl = await minioClient.presignedGetObject(
         BUCKET_NAME,
         objectName,
         60 * 60 * 24,
       );
+
       return { qrCodeUrl };
     } catch {
       throw new Error("Failed to fetch the QR code URL.");
