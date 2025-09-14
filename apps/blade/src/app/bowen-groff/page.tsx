de059fe14f21001850a9d09270c1f1abf8479a09
bowen <botgroff@gmail.com>
2025-09-14 02:50:26 -0400

    Added icons

--- apps/blade/src/app/bowen-groff/page.tsx
+++ apps/blade/src/app/bowen-groff/page.tsx
@@ -5,6 +5,9 @@ import { useState, useEffect, useCallback, useMemo } from 'react';
 import './globals.css';
 import localFont from "next/font/local";
 import startButtonIcon from './assets/start-button.png';
+import recycleIcon from './assets/recycle.ico';
+import computerIcon from './assets/computer.ico';
+
 const windows95 = localFont({
   src: './assets/Windows Regular.ttf',
   display: "swap",
@@ -33,10 +36,18 @@ function Page() {
 return (
   <div className={`${windows95.className}`}>
     <div className="w-screen h-screen bg-[#008080] flex flex-wrap content-start p-4 gap-8">
-      <div className="windows-base taskbar">
-        <button ><img className="startbutton" src={startButtonIcon.src}></img></button>
+      <div className="icon-column">
+        <button className="icon"><img src={computerIcon.src}></img>
+          My Computer
+        </button>
+        <button className="icon"><img src={recycleIcon.src}></img>
+          Recycle Bin
+        </button>
+      </div>
+      <div className="convex taskbar">
+        <button className="icon"><img className="startbutton" src={startButtonIcon.src}></img></button>
         <div className="spacer"></div>
-        <div className="windows-base clock">{Clock()}</div>
+        <div className="concave clock">{Clock()}</div>
       </div>
     </div>
   </div>
