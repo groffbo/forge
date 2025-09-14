ef06c4ed36c2ad6834d13b80032c1a149f7c4bce
bowen <botgroff@gmail.com>
2025-09-14 17:09:11 -0400

    Created window logic and added tabs. welcome window finished

--- apps/blade/src/app/bowen-groff/page.tsx
+++ apps/blade/src/app/bowen-groff/page.tsx
@@ -2,11 +2,23 @@
 
 import { useState, useEffect, useCallback, useMemo } from 'react';
 
+import Window from './_components/window';
+
 import './globals.css';
 import localFont from "next/font/local";
 import startButtonIcon from './assets/start-button.png';
 import recycleIcon from './assets/recycle.ico';
 import computerIcon from './assets/computer.ico';
+import textFileIcon from './assets/textfile.ico';
+import photo from './assets/me.jpg';
+import crunch from './assets/crunchcat.gif';
+
+interface AppWindow { id: string; 
+                      title: string; 
+                      isOpen: boolean; 
+                      startX?: number;
+                      startY?: number;
+                    }
 
 const windows95 = localFont({
   src: './assets/Windows Regular.ttf',
@@ -32,6 +44,15 @@ const Clock = () => {
 };
 
 function Page() {
+const [windows, setWindows] = useState<AppWindow[]>([
+  { id: "welcome", title: "Hello World", isOpen: true },
+  { id: "resume", title: "Resume.txt", isOpen: false },
+]);
+
+const toggleWindow = (id: string) => {
+  setWindows(prev => prev.map(w => (w.id === id ? { ...w, isOpen: !w.isOpen} : w))
+  );
+};
 
 return (
   <div className={`${windows95.className}`}>
@@ -43,13 +64,69 @@ return (
         <button className="icon"><img src={recycleIcon.src}></img>
           Recycle Bin
         </button>
+        <button className="icon" onClick={() => toggleWindow("resume")}><img src={textFileIcon.src}></img>
+          Resume.txt
+        </button>
       </div>
       <div className="convex taskbar">
-        <button className="icon"><img className="startbutton" src={startButtonIcon.src}></img></button>
-        <div className="spacer"></div>
+        <button className="icon"><img className="startbutton" src={startButtonIcon.src}></img>
+        </button>
+        <div className="spacer">
+          {windows.map(w => (
+              <button 
+                key={w.id} 
+                className="convex tab" 
+                onClick={() => toggleWindow(w.id)}
+              >
+                {w.title}
+              </button>
+            ))}
+        </div>
         <div className="concave clock">{Clock()}</div>
       </div>
     </div>
+    {windows.map(w =>
+      w.isOpen ? (
+        <Window 
+          key={w.id}
+          title={w.title}
+          startX={20 + Math.random() * 100}
+          startY={50 + Math.random() * 50}
+          onClose={() => toggleWindow(w.id)}>
+            {w.id === "welcome" ? (
+              <>
+                <div className="flex gap-4">
+                  <img className="convex image" src={photo.src}/>
+                  <span className="text mt-8">
+                    My name is Bowen Groff. This is my second year at UCF.
+                    I am a Computer Engineering student with an interest in anything software. 
+                    I have worked on...
+                    <ul className="list-disc pl-6 ">
+                      <li>Kernel Module/Driver Development</li>
+                      <li>Full Stack web dev w/ ReactJS</li>
+                      <li>AI/ML with Python using Pytorch and Tensorflow</li>
+                      <li>...Plenty more with more to come!</li>
+                    </ul>
+                    <img className="h-12" src={crunch.src}></img>
+                    <div className="absolute ml-5 left-0 bottom-0">
+                      Try double clicking stuff! And feel free to close this window :-)
+                    </div>
+                  </span>
+                </div>
+              </>
+            ) : w.id === "resume" ? (
+              <iframe src="assets/bowen-groff-resume.pdf" 
+                      title="Resume"
+                      width="100%"
+                      height="100%">
+              </iframe>
+
+            ) : (
+              <p></p>
+            )}
+        </Window>
+      ) : null
+    )}
   </div>
   );
 }
